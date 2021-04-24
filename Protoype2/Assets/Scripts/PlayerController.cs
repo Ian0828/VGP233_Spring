ebbcabe7c1fe3f626160af04358ddb5fb5f5e7e2
Ian Ma <justinma888@gmail.com>
2021-04-23 23:17:08 -0700

    Assignment 2 Done!!!

---
+++ Protoype2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float horizontalInput;
+    private float xRange = 19.0f;
+
+    public float speed = 10.0f;
+    public GameObject projectilePrefab;
+    public float projectileOffset;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // left and right
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * speed * Time.deltaTime);
+
+        // keep the player in bounds
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            // launch a hamburger/food/projectile
+            Instantiate(projectilePrefab, new Vector3(transform.position.x, transform.position.y + projectileOffset, transform.position.z), projectilePrefab.transform.rotation);
+        }
+    }
+}
---
+++ YungHsiang_Ma_Assignment_2/Assets/Assignment2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+    public int maxDog = 1;
+    private int currentDog;
+    private float reloaTime = 1.5f;
+    private bool isReloading = false;
+
+    void Start()
+    {
+        currentDog = maxDog;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (isReloading)
+        {
+            return;
+        }
+
+        if (currentDog <=0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            Go();
+        }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+
+        yield return new WaitForSeconds(reloaTime);
+
+        currentDog = maxDog;
+        isReloading = false;
+    }
+
+    void Go()
+    {
+        currentDog--;
+    }
+
+    
+}
